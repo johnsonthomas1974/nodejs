6c8379e11916f88e7bfb0ae544c36ff3ff7f977e
rhematree <rhematree@gmail.com>
2015-10-18 17:20:13 +0100

    adding soap response for the nodejs test service..

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ var express = require('express');
 var PORT = 8080;
 // App
 var app = express();
+app.post('/', function (req, res) {
+  res.send('<?xml version="1.0" encoding="UTF-8"?><soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"><soapenv:Body><ns:soapHelloResponse  xmlns:ns="http://services.samples/xsd">Hello World !! </ns:soapHelloResponse></soapenv:Body></soapenv:Envelope>');
+});
+app.post('/soapHello', function (req, res) {
+  res.send('<?xml version="1.0" encoding="UTF-8"?><soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"><soapenv:Body><ns:soapHelloResponse  xmlns:ns="http://services.samples/xsd">Hello World !! </ns:soapHelloResponse></soapenv:Body></soapenv:Envelope>');
+});
+
 app.get('/', function (req, res) {
   res.send('++++ Hello world ++++ Message Changed++ \n');
 });
-
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
