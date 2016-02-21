a414c1fd26f6bd3e99eff080ff7659de336102f8
johnsonthomas1974 <johnsonthomas1974@gmail.com>
2016-02-21 20:24:27 +0000

    Update index.js

--- index.js
+++ index.js
@@ -17,26 +17,17 @@ app.get('/', function (req, res) {
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
 function getDateTime() {
-
     var date = new Date();
-
     var hour = date.getHours();
     hour = (hour < 10 ? "0" : "") + hour;
-
     var min  = date.getMinutes();
     min = (min < 10 ? "0" : "") + min;
-
     var sec  = date.getSeconds();
     sec = (sec < 10 ? "0" : "") + sec;
-
     var year = date.getFullYear();
-
     var month = date.getMonth() + 1;
     month = (month < 10 ? "0" : "") + month;
-
     var day  = date.getDate();
     day = (day < 10 ? "0" : "") + day;
-
     return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
-
 }
